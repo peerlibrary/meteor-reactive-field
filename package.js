56fccebc8ba99347b158001c2a3a2225ddfcbfb6
Mitar <mitar.git@tnode.com>
2019-09-20 11:32:28 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'peerlibrary:reactive-field',
   summary: "Reactive field for Meteor",
-  version: '0.5.0',
+  version: '0.6.0',
   git: 'https://github.com/peerlibrary/meteor-reactive-field.git'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.4.4.5');
+  api.versionsFrom('METEOR@1.8.1');
 
   // Core dependencies.
   api.use([
@@ -22,16 +22,16 @@ Package.onUse(function (api) {
 });
 
 Package.onTest(function (api) {
-  api.versionsFrom('METEOR@1.4.4.5');
+  api.versionsFrom('METEOR@1.8.1');
 
   // Core dependencies.
   api.use([
-    'coffeescript@2.0.3_3',
+    'coffeescript@2.4.1',
     'ecmascript',
     'tracker',
-    'templating',
-    'blaze',
-    'spacebars',
+    'templating@1.3.2',
+    'blaze@2.3.3',
+    'spacebars@1.0.15',
     'underscore'
   ]);
 
@@ -42,7 +42,7 @@ Package.onTest(function (api) {
 
   // 3rd party dependencies.
   api.use([
-    'peerlibrary:classy-test@0.3.0'
+    'peerlibrary:classy-test@0.4.0'
   ]);
 
   api.addFiles([
